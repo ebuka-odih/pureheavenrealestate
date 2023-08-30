c476b9ffcda9b84e83921fded1eee4ec69bd05b6
Gnosis <emmagab38@gmail.com>
2023-08-30 13:26:58 +0100

    commit changes

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -34,58 +34,65 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-sm-12 col-md-12">
-                            <div class="card">
-                                <div class="card-body">
-                                    <div class="media align-items-center">
-                                        <div class="media-body me-3">
-                                            <h2 class="fs-36 text-black font-w600">{{ $total_property }}</h2>
-                                            <p class="fs-18 mb-0 text-black font-w500">Listed Properties</p>
-                                        </div>
-                                        <div class="d-inline-block position-relative donut-chart-sale">
-                                            <span class="donut1" data-peity='{ "fill": ["rgb(60, 76, 184)", "rgba(236, 236, 236, 1)"],   "innerRadius": 38, "radius": 10}'>8/8</span>
-                                            <small class="text-primary">Active</small>
-                                            <span class="circle bgl-success"></span>
-                                        </div>
+                        
+                    </div>
+                </div>
+                <div class="row">
+                    
+                    <div class="col-sm-12 col-md-12 col-lg-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="media align-items-center">
+                                    <div class="media-body me-3">
+                                        <h2 class="fs-36 text-black font-w600">{{ $total_property }}</h2>
+                                        <p class="fs-18 mb-0 text-black font-w500">Listed Properties</p>
+                                    </div>
+                                    <div class="d-inline-block position-relative donut-chart-sale">
+                                        <span class="donut1" data-peity='{ "fill": ["rgb(60, 76, 184)", "rgba(236, 236, 236, 1)"],   "innerRadius": 38, "radius": 10}'>8/8</span>
+                                        <small class="text-primary">Active</small>
+                                        <span class="circle bgl-success"></span>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-xl-6 col-xxl-12">
-                    <div class="card">
-                        <div class="card-header border-0 pb-0">
-                            <h3 class="fs-20 text-black">Balance</h3>
-
-                        </div>
-                        <div class="card-body pt-2 pb-0">
-                            <div class="d-flex flex-wrap align-items-center">
-                                <span class="fs-36 text-black font-w600 me-3">$@money(auth()->user()->balance)</span>
-                                <p class="me-sm-auto me-3 mb-sm-0 mb-3">Profit $@money(auth()->user()->profit)</p>
-                                <div class="d-flex align-items-center">
-                                    <svg class="me-3" width="87" height="47" viewBox="0 0 87 47" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M29.8043 20.9254C15.2735 14.3873 5.88029 27.282 3 34.5466V46.2406H85V4.58005C70.8925 -0.868404 70.5398 8.66639 60.8409 19.5633C51.1419 30.4602 47.9677 29.0981 29.8043 20.9254Z" fill="url(#paint0_linear)"/>
-                                        <path d="M3 35.2468C5.88029 27.9822 15.2735 15.0875 29.8043 21.6257C47.9677 29.7984 51.1419 31.1605 60.8409 20.2636C70.5398 9.36665 70.8925 -0.168147 85 5.28031" stroke="#37D159" stroke-width="6"/>
-                                        <defs>
-                                            <linearGradient id="paint0_linear" x1="44" y1="-36.4332" x2="44" y2="45.9686" gradientUnits="userSpaceOnUse">
-                                                <stop stop-color="#37D159" offset="0"/>
-                                                <stop offset="1" stop-color="#37D159" stop-opacity="0"/>
-                                            </linearGradient>
-                                        </defs>
-                                    </svg>
-                                    <span class="fs-22 text-success me-2"></span>
-                                    <svg width="12" height="6" viewBox="0 0 12 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M0 6L6 2.62268e-07L12 6" fill="#37D159"/>
-                                    </svg>
-
-                                </div>
-                                <canvas id="widgetChart1" class="max-h80 chartjs-render-monitor" style="display: block; width: 374px; height: 80px;" width="374" height="80"></canvas>
+                    
+                    <div class="col-xl-6 col-xxl-6 col-md-12">
+                        <div class="card">
+                            <div class="card-header border-0 pb-0">
+                                <h3 class="fs-20 text-black">Balance</h3>
+    
                             </div>
-
+                            <div class="card-body pt-2 pb-0">
+                                <div class="d-flex flex-wrap align-items-center">
+                                    <span class="fs-36 text-black font-w600 me-3">$@money(auth()->user()->balance)</span>
+                                    <p class="me-sm-auto me-3 mb-sm-0 mb-3">Profit $@money(auth()->user()->profit)</p>
+                                    <div class="d-flex align-items-center">
+                                        <svg class="me-3" width="87" height="47" viewBox="0 0 87 47" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M29.8043 20.9254C15.2735 14.3873 5.88029 27.282 3 34.5466V46.2406H85V4.58005C70.8925 -0.868404 70.5398 8.66639 60.8409 19.5633C51.1419 30.4602 47.9677 29.0981 29.8043 20.9254Z" fill="url(#paint0_linear)"/>
+                                            <path d="M3 35.2468C5.88029 27.9822 15.2735 15.0875 29.8043 21.6257C47.9677 29.7984 51.1419 31.1605 60.8409 20.2636C70.5398 9.36665 70.8925 -0.168147 85 5.28031" stroke="#37D159" stroke-width="6"/>
+                                            <defs>
+                                                <linearGradient id="paint0_linear" x1="44" y1="-36.4332" x2="44" y2="45.9686" gradientUnits="userSpaceOnUse">
+                                                    <stop stop-color="#37D159" offset="0"/>
+                                                    <stop offset="1" stop-color="#37D159" stop-opacity="0"/>
+                                                </linearGradient>
+                                            </defs>
+                                        </svg>
+                                        <span class="fs-22 text-success me-2"></span>
+                                        <svg width="12" height="6" viewBox="0 0 12 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M0 6L6 2.62268e-07L12 6" fill="#37D159"/>
+                                        </svg>
+    
+                                    </div>
+                                    <canvas id="widgetChart1" class="max-h80 chartjs-render-monitor" style="display: block; width: 374px; height: 80px;" width="374" height="80"></canvas>
+                                </div>
+    
+                            </div>
                         </div>
                     </div>
+
                 </div>
+                
 
             </div>
         </div>
